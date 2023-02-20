07723d510263b73a13af378270cef41b47787187
Benjamin Glusa <benjamin.glusa@volkswagen.de>
2023-02-20 19:40:30 +0100

    initial project structure

---
+++ pkg/random/strings.go
@@ -0,0 +1,13 @@
+package random
+
+import (
+	"math/rand"
+)
+
+func GenerateString(l int) string {
+	bytes := make([]byte, l)
+	for i := 0; i < l; i++ {
+		bytes[i] = byte(65 + rand.Intn(90-65))
+	}
+	return string(bytes)
+}
